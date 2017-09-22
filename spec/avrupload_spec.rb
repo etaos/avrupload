ca36e4a4a71dda914d439ea8446ed2f5ca080518
Michel Megens <dev@michelmegens.net>
2017-09-22 14:29:46 +0200

    initial commit: add project setup
    Add a Ruby gem project setup.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ spec/avrupload_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Avrupload do
+  it "has a version number" do
+    expect(Avrupload::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
